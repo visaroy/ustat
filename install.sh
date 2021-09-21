5152e8bb7774c95237af94e0b9f8dc15b94b6a54
porczynski <dariusz@porczynski.net>
2021-09-21 19:47:11 +0000

    🐸 something new

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# install ustat service
+
+case $1 in
+    "--force")
+    rm /etc/ustat
+    rm /usr/bin/ustat
+    rm /etc/init.d/ustat
+    ;;
+esac
+
+if [ -f /etc/ustat ]; then
+    echo "[OK] ustat config file already in /etc/ustat"
+else
+    cp ./etc/ustat /etc/ustat
+    echo "[OK] ustat config file coppied to /etc/ustat"
+fi
+
+if [ -x /usr/bin/ustat ]; then
+    echo "[OK] ustat script already installed"
+else
+    cp ./ustat /usr/bin/ustat
+    chmod +x /usr/bin/ustat
+    echo "[OK] ustat script installed"
+fi
+
+if [ -x /etc/init.d/ustat ]; then
+    echo "[OK] ustat service already installed, usage: /etc/init.d/ustat start"
+else
+    cp ./etc/init.d/ustat /etc/init.d/ustat
+    chmod +x /etc/init.d/ustat
+    cd /etc/init.d/
+    update-rc.d -v ustat start 99 3 . stop 99 6 . >/dev/null
+    update-rc.d -f ustat defaults 99 >/dev/null
+    cd /
+    echo "[OK] ustat service installed, usage: service ustat start"
+fi
+
+echo "use 'install.sh --force' if you want to reinstall files..."
